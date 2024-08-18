8c5f0821caf7e4051d5eb64fd12650860cb3780b
kishankumarkk08 <kkcodes.08@gmail.com>
2024-08-18 17:39:09 +0530

    major commit

---
+++ src/lib/auth.ts
@@ -0,0 +1,55 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { db } from "./db";
+import { compare } from "bcrypt";
+
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt"
+  },
+  pages: {
+    signIn: '/sign-in',
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const existingUser = await db.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+
+        if (!existingUser) {
+          return null;
+        }
+
+        const checkPassword = await compare(credentials.password, existingUser.password);
+
+        if (!checkPassword) {
+          return null
+        }
+
+        return {
+          id: `${existingUser.id}`,
+          email: existingUser.email,
+          username: existingUser.username
+        }
+
+
+      }
+    })
+  ]
+}
